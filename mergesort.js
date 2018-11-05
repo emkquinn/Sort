8381555fb6ec0176c72eea0e575fb635c3fe0349
Emily Quinn <emkquinn@gmail.com>
2018-11-05 16:25:01 -0500

    pair programming

--- mergesort.js
+++ mergesort.js
@@ -1,15 +1,35 @@
 function split(wholeArray) {
-  let temp = wholeArray.length / 2;
-  let firstHalf = wholeArray.slice(0, temp);
-  let secondHalf = wholeArray.slice(temp);
-  return [firstHalf, secondHalf];
+  for (let i = 0; i < wholeArray.length; i++) {
+    // if (Array.isArray(wholeArray[i])) {
+    //   return split(wholeArray[i]);
+    // } else {
+      let temp = wholeArray.length / 2;
+      let firstHalf = wholeArray.slice(0, temp);
+      let secondHalf = wholeArray.slice(temp);
+      return [firstHalf, secondHalf];
+  //   }
+  }
 }
 
-function merge(arr1, arr2){
+function merge(arr1, arr2) {
   return [...arr1, ...arr2];
 }
 
 function mergeSort(array) {
-  /* your code here */
+  let splitArr = split(array);
+  let tempArr = [];
+  if (array.length === 1) return array;
+  if (splitArr.length === array.length) {
+    for (let i = 0; i < splitArr.length; i++) {
+      let j = i + 1;
+      if (splitArr[i] > splitArr[j]) {
+        tempArr = merge(splitArr[i], splitArr[j]);
+      } else {
+        tempArr = merge(splitArr[j], splitArr[i]);
+      }
+      splitArr = splitArr.splice(i, 2, tempArr);
+    }
+  } else {
+      return splitArr.map(elem => split(elem));
+  }
 }
-
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -7,7 +7,7 @@ describe('Split Array function', function() {
     expect(splitR.length).toEqual(2);
   });
   it('is able to split an array with an odd number of elements', function() {
-    expect(split([1, 2, 3])).toEqual([[1], [2,3]]);
+    expect(split([1, 2, 3])).toEqual([[1], [2, 3]]);
     expect(split([1, 2, 3]).length).toEqual(2);
     const unsplit = [5, 9, 1, 18, 3];
     const splitR = split(unsplit);
@@ -27,9 +27,30 @@ describe('Merge function', function() {
 describe('MergeSort function', function() {
   it('is able to sort an unsorted array', function() {
     expect(mergeSort([2])).toEqual([2]);
-    expect(mergeSort([3,1])).toEqual([1,3]);
-    expect(mergeSort([3,2,6,1,0,15])).toEqual([0,1,2,3,6,15]);
-    expect(mergeSort([5,9,2,15,6])).toEqual([2,5,6,9,15]);
-    expect(mergeSort([3, -8, 36, -18])).toEqual([-18,-8,3,36]);
+    expect(mergeSort([3, 1])).toEqual([1, 3]);
+    expect(mergeSort([3, 2, 6, 1, 0, 15])).toEqual([0, 1, 2, 3, 6, 15]);
+    expect(mergeSort([5, 9, 2, 15, 6])).toEqual([2, 5, 6, 9, 15]);
+    expect(mergeSort([3, -8, 36, -18])).toEqual([-18, -8, 3, 36]);
+  });
+  it('should clumsily sort strings', function() {
+    const unsorted = ['toad', 'prune', 'pretzel', 'aardvark', 'tuna', 'weasel'];
+    const sorted = mergeSort(unsorted);
+    expect(sorted).toEqual([
+      'aardvark',
+      'pretzel',
+      'prune',
+      'toad',
+      'tuna',
+      'weasel',
+    ]);
+    expect(sorted.length).toEqual(6);
+  });
+  it('does not use built-in sorting methods', function() {
+    spyOn(Array.prototype, 'sort').and.callThrough();
+    const unsorted = [107, -443, 216, 300, -9, 247];
+    const sorted = mergeSort(unsorted);
+    expect(sorted).toEqual([-443, -9, 107, 216, 247, 300]);
+    expect(sorted.length).toEqual(6);
+    expect(Array.prototype.sort.calls.count()).toEqual(0);
   });
 });
